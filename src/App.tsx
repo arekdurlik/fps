82d79c388e4b692c0a17221598aab876c13357da
arekdurlik <72689308+arekdurlik@users.noreply.github.com>
2024-06-17 17:03:15 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+function App() {
+
+  return (
+    <Canvas shadows>
+      <group>
+      </group>
+    </Canvas>
+  )
+}
+
+export default App
